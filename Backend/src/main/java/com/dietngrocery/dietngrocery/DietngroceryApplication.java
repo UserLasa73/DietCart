0df56053bf428c5d80d1c055bef907cd07ab867f
LasanthaKumara <lasanthakumaraofficial@gmail.com>
2025-04-22 22:50:30 +0530

    Created seperate folders forfrontend and backend. extracted Springboot project into backend

---
+++ Backend/src/main/java/com/dietngrocery/dietngrocery/DietngroceryApplication.java
@@ -0,0 +1,13 @@
+package com.dietngrocery.dietngrocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietngroceryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietngroceryApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/dietngrocery/dietngrocery/DietngroceryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dietngrocery.dietngrocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietngroceryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
