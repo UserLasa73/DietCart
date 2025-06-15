91099687aaca1233835e5fef665cf22ed44fc8f8
LasanthaKumara <lasanthakumaraofficial@gmail.com>
2025-06-15 23:15:40 +0530

    Added filtering with custom query

--- Backend/src/main/java/com/dietcart/dietcart/Repository/ProductsRepository.java
+++ Backend/src/main/java/com/dietcart/dietcart/Repository/ProductsRepository.java
@@ -1,7 +1,15 @@
 package com.dietcart.dietcart.Repository;
 
 import com.dietcart.dietcart.model.Products;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductsRepository extends JpaRepository<Products, Long> {
+    @Query("SELECT DISTINCT p FROM Products p JOIN p.dietTypes d WHERE " +
+            "(:dietTypeIds IS NULL OR d.id IN :dietTypeIds)")
+    List<Products> findByDietTypes(@Param("dietTypeIds") List<Long> dietTypeIds);
 }
