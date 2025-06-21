f7116214c761504c06771712e786bdd57f661cd0
LasanthaKumara <lasanthakumaraofficial@gmail.com>
2025-06-21 13:41:57 +0530

    created DTOs to be used in AuthController

---
+++ Backend/src/main/java/com/dietcart/dietcart/dto/MessageResponse.java
@@ -0,0 +1,11 @@
+package com.dietcart.dietcart.dto;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() { return message; }
+}
