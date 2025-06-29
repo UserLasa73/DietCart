3dd5370c77ba9c7499bde16a14c3dfae67fe441b
LasanthaKumara <lasanthakumaraofficial@gmail.com>
2025-06-29 17:33:47 +0530

    error fix

--- Backend/src/main/java/com/dietcart/dietcart/model/User.java
+++ Backend/src/main/java/com/dietcart/dietcart/model/User.java
@@ -28,7 +28,7 @@ public class User {
 
     public User(String name, String email, String password, String role) {
         this.name = name;
-        this.email = email;
+        this.email = email; 
         this.password = password;
         this.role = role;
     }
